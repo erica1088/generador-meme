60601daca4f1b78b995bf913e262edeffa4b2d77
ericaacosta <acosta.s.erica@gmail.com>
2024-04-02 22:00:24 -0300

    modificaciones en html y css

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const botonImg =document.getElementById("boton-img");
 
 const botonText =document.getElementById("boton-texto");
 
+
 const asideImg = document.getElementById("asideimagen");
 const asideText = document.getElementById("asidetext");
-const header= document.getElementById("myheader")
+const header= document.getElementById("myheader");
+const titulo = document.getElementById("titulo-principal");
+const asidePanel = document.getElementById("panel-aside");
 
 botonImg.addEventListener("click",()=>{
     asideText.style.display="none";
@@ -24,18 +27,24 @@ let cambioModo = "Modo Oscuro";
 botonModo.addEventListener("click",()=>{ 
 
     if (cambioModo === "modo claro"){
-    document.body.style.background= "grey";
-    document.body.style.color = "white";
-    header.style.background= "pink";
+    document.body.style.background= "#BFC9CA";
+    document.body.style.color = "#D7DBDD";
+    header.style.background= "#85929E ";
+    titulo.style.color = "white";
+    asidePanel.style.background= "#AAB7B8";
     botonModo.textContent=cambioModo;
     cambioModo="Modo Oscuro";
 }else if (cambioModo === "Modo Oscuro"){
-    document.body.style.background= "blue";
-    document.body.style.color = "white";
-    header.style.background= "green";
+    document.body.style.background= "#5B2C6F ";
+    document.body.style.color = "black ";
+    header.style.background= "#4A235A";
+    titulo.style.color = "black"
+    asidePanel.style.background= "#512E5F"
     botonModo.textContent=cambioModo;
     cambioModo="modo claro";
 
 }
 
 });
+
+
